8d397c03d43af4c7f28625b7b2a54a44dfb54e81
vishn <vishnupriya.chimata@wsu.edu>
2022-02-01 03:53:45 -0800

    Add project files.

---
+++ Milestonee1/MainWindow.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+
+
+namespace Milestonee1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Business
+        {
+            public string bid { get; set; }
+            public string name { get; set; }
+            public string state { get; set; }
+            public string city { get; set; }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            addState();
+            addColumns2Grid();
+            buildConnectionString();
+
+        }
+
+        private string buildConnectionString()
+        {
+            return "Host = localhost; Username = postgres; Database = milestone1db; password= 0722";
+        }
+        private void addState ()
+        {
+            using (var connection = new NpgsqlConnection(buildConnectionString()))
+            {
+                connection.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+
+                    cmd.Connection = connection;
+                    cmd.CommandText = "SELECT distinct state FROM business ORDER by state";
+                    try
+                    {
+                        var reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                            stateList.Items.Add(reader.GetString(0));
+
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        Console.WriteLine(ex.Message.ToString());
+                        System.Windows.MessageBox.Show("SQL error - " + ex.Message.ToString());
+
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+            
+        }
+        private void addColumns2Grid()
+        {
+            DataGridTextColumn col1 = new DataGridTextColumn();
+            col1.Binding = new Binding("name");
+            col1.Header = "BusinessName";
+            col1.Width = 255;
+            businessGrid.Columns.Add(col1);
+
+            DataGridTextColumn col2 = new DataGridTextColumn();
+            col2.Binding = new Binding("state");
+            col2.Header = "State";
+            col2.Width = 60;
+            businessGrid.Columns.Add(col2);
+
+            DataGridTextColumn col3 = new DataGridTextColumn();
+            col3.Binding = new Binding("city");
+            col3.Header = "City";
+            col3.Width = 150;
+            businessGrid.Columns.Add(col3);
+
+            DataGridTextColumn col4 = new DataGridTextColumn();
+            col4.Binding = new Binding("bid");
+            col4.Header = "";
+            col4.Width = 0;
+            businessGrid.Columns.Add(col4);
+
+
+        }
+
+
+   
+        private void executeQuery(string sqlstr, Action<NpgsqlDataReader> myf)
+        {
+            using (var connection = new NpgsqlConnection(buildConnectionString()))
+            {
+                connection.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+
+                    cmd.Connection = connection;
+                    cmd.CommandText = sqlstr;
+                    try
+                    {
+                        var reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                            myf(reader);
+                            
+
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        Console.WriteLine(ex.Message.ToString());
+                        System.Windows.MessageBox.Show("SQL error - " + ex.Message.ToString());
+
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+        private void addCity(NpgsqlDataReader R)
+        {
+            cityList.Items.Add(R.GetString(0));
+
+        }
+
+        private void stateList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cityList.Items.Clear();
+            if (stateList.SelectedIndex > -1)
+            {
+
+
+
+                //string sqlStr = "SELECT distinct city FROM business WHERE state ='" + stateList.SelectedItem.ToString() + "'ORDER BY city";
+                string sqlStr = $"SELECT distinct city FROM business WHERE state = '{stateList.SelectedItem.ToString()}' ORDER BY city;";
+
+                executeQuery(sqlStr, addCity);
+
+            }
+        }
+        private void addGridRow(NpgsqlDataReader R)
+        {
+            businessGrid.Items.Add(new Business() { name = R.GetString(0), state = R.GetString(1), city = R.GetString(2), bid = R.GetString(3) });
+
+        }
+        private void cityList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            businessGrid.Items.Clear();
+            if (cityList.SelectedIndex > -1)
+            {
+                //string sqlStr = "SELECT name,state,city,business_id FROM business WHERE state ='" + stateList.SelectedItem.ToString() + "' AND city = '" + cityList.SelectedItem.ToString() + "' ORDER BY name;";
+                string sqlStr = $"SELECT name,state,city,business_id FROM business WHERE state = '{stateList.SelectedItem.ToString()}' AND city = '{cityList.SelectedItem.ToString()}';";
+
+                executeQuery(sqlStr,addGridRow);
+
+                
+            }
+
+        }
+
+        private void businessGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (businessGrid.SelectedIndex > -1)
+            {
+                Business B = businessGrid.Items[businessGrid.SelectedIndex] as Business;
+                if ((B.bid != null) && (B.bid.ToString().CompareTo("") != 0))
+                        {
+                    Window1 businessWindow = new Window1(B.bid.ToString());
+                    businessWindow.Show();
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Milestonee1/Window1.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Npgsql;
+
+namespace Milestonee1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private string bid = "";
+        public Window1(string bid)
+        {
+            InitializeComponent();
+            this.bid = String.Copy(bid);
+            loadBusinessDetails();
+            loadBusinessNumbers();
+
+        }
+        private string buildConnectionString()
+        {
+            return "Host = localhost; Username = postgres; Database = milestone1db; password= 0722";
+        }
+        private void executeQuery(string sqlstr, Action<NpgsqlDataReader> myf)
+        {
+            using (var connection = new NpgsqlConnection(buildConnectionString()))
+            {
+                connection.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+
+                    cmd.Connection = connection;
+                    cmd.CommandText = sqlstr;
+                    try
+                    {
+                        var reader = cmd.ExecuteReader();
+                        reader.Read();
+                        myf(reader);
+
+
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        Console.WriteLine(ex.Message.ToString());
+                        System.Windows.MessageBox.Show("SQL error - " + ex.Message.ToString());
+
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+        private void setBusinessDetails(NpgsqlDataReader R)
+        {
+            bname.Text = R.GetString(0);
+            state.Text = R.GetString(1);
+            city.Text = R.GetString(2);
+
+        }
+        void setNumInState(NpgsqlDataReader R)
+        {
+            numInState.Content = R.GetInt16(0).ToString();
+        }
+        void setNumInCity(NpgsqlDataReader R)
+        {
+            numInCity.Content = R.GetInt16(0).ToString();
+        }
+        private void loadBusinessNumbers()
+        {
+            //string sqlStr1 = "SELECT count(*) from business WHERE state = (SELECT state FROM business WHERE business_id = '" + this.bid + "');";
+            string sqlStr1 = $"SELECT count(*) from business WHERE state = (SELECT state FROM business WHERE business_id = '{this.bid}');";
+            executeQuery(sqlStr1, setNumInState);
+            //string sqlStr2 = "SELECT COUNT(*) from business WHERE city = (SELECT city FROM business WHERE business_id = '" + this.bid + "');";
+            string sqlStr2 = $"SELECT count(*) from business WHERE city = (SELECT city FROM business WHERE business_id = '{this.bid}');";
+            executeQuery(sqlStr2, setNumInCity);
+
+        }
+
+        private void loadBusinessDetails()
+        {
+            //string sqlStr = "SELECT name,state,city FROM business WHERE business_id = '" + this.bid + "';";
+            string sqlStr = $"SELECT name,state,city FROM business WHERE business_id = '{this.bid}';";
+            executeQuery(sqlStr, setBusinessDetails);
+        }
+
+
+    }
+    
+}
+
+    
+
+
+     
